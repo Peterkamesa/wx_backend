e5d8a03aecd05de9adfb1eb3c0e3acef170d4422
Peterkamesa <petmaish1@gmail.com>
2025-07-24 06:33:49 +0300

    added station number

--- models/report.js
+++ models/report.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const reportSchema = new mongoose.Schema({
     content: {type: String, required: true},
     type: {type: String, required: true, enum: ['METAR', 'SYNOP', 'ACTUALS']},
+    station: {
+        name: { type: String, required: true },
+        number: { type: String, required: true }
+    },
     createdAt: {type: Date, default: Date.now},
 });
 
