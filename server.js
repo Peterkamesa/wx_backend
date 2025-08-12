18a04aa319030904973d71ab88b6d14f1f6f9f3b
Peterkamesa <petmaish1@gmail.com>
2025-08-12 10:11:40 +0300

    modified the cors

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(cors({
     'https://peterkamesa.github.io',  // Remove trailing slash and path
     'https://wxbackend-production.up.railway.app',
     'http://localhost:3001',
-    'http://localhost:5502',
+    'http://127.0.0.1:5502',
     'http://127.0.0.1:5500'
   ],
   methods: ['GET', 'POST', 'DELETE'],
